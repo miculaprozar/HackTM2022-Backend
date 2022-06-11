7a821dcd722f510f4ecb0d3d78930fba9a011056
Valentin Gorcea <valentingorcea@gmail.com>
2022-06-11 17:34:52 +0300

    add image insertion in db

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -6,6 +6,22 @@ const checkAuth = require("../middlewares/check-auth");
 const { check, validationResult } = require("express-validator");
 const addParameters = require("../util/addParameters");
 const log = require("../services/logService");
+const sharp = require('sharp');
+const path = require('path');
+const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage, limits: { files: 3 } });
+const googleCloud = new Storage({
+  keyFilename: path.join(
+    __dirname,
+    `../../storage.json`
+  ),
+  projectId: "civic-badge-262911",
+});
+const photosBucket = googleCloud.bucket(
+  "farmtofork"
+);
 
 let productRouter = express.Router();
 
@@ -78,6 +94,63 @@ let router = function (productService, webConstants) {
       } catch (error) {
         log.error(error.message || error);
         return next(new HttpError(500, error.message || error));
+      }
+    }
+  );
+
+  productRouter.post(
+    '/image/:id',
+    upload.any('inputFile'),
+    checkAuth,
+    async (req, res, next) => {
+      try {
+        if (req.files[0].size > 734003200) {
+          throw new HttpError(403, 'Dimensiunea fisierului este prea mare!');
+        }
+        if (!req.params.id) {
+          throw new HttpError(403, 'Please provide product id!');
+        }
+
+        const product = await productService.getProducts(
+          req.params.id,
+          req.userData.userId
+        );
+
+        if (!product) {
+          throw new HttpError('403', 'You thief! This product is not yours');
+        }
+
+        const blob = photosBucket.file(req.params.id);
+        const blobStream = blob.createWriteStream({
+          metadata: {
+            contentType: req.files[0].mimetype,
+            cacheControl: 'no-cache, max-age=0',
+          },
+        });
+
+        blobStream.on('error', (err) => {
+          return next(err);
+        });
+
+        blobStream.on('finish', async () => {
+
+          res.setHeader('Status', 200);
+          res.send("Imagine introdusa cu succes!");
+        });
+
+        sharp(req.files[0].buffer, { failOnError: false })
+          .rotate()
+          .resize(500)
+          .toBuffer()
+          .then((data) => {
+            blobStream.end(data);
+          })
+          .catch((err) => {
+            throw new HttpError(500, `Compression failed with error: ${err}`);
+          });
+      } catch (err) {
+        log.error(JSON.stringify(err));
+        return next(err);
       }
     }
   );
